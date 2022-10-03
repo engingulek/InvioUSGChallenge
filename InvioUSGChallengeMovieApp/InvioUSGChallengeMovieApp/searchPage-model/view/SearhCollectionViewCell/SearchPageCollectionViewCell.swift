c279e963e9800359ae05ecf1480e376d716f65a7
Engin Gülek <engingulek0@gmail.com>
2022-10-03 15:29:55 +0300

    MovieCollectionViewCell:Create MovieCollectionViewCell.xib

---
+++ InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/searchPage-model/view/SearhCollectionViewCell/SearchPageCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchPageCollectionViewCell.swift
+//  InvioUSGChallengeMovieApp
+//
+//  Created by engin gülek on 3.10.2022.
+//
+
+import UIKit
+
+class SearchPageCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
