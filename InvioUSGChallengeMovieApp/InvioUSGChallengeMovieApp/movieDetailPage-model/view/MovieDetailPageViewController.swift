6aec85bf9ce8632975c011f8555ccd19a35e3b85
Engin Gülek <engingulek0@gmail.com>
2022-10-04 21:39:29 +0300

    Animation Start

--- InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/movieDetailPage-model/view/MovieDetailPageViewController.swift
+++ InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/movieDetailPage-model/view/MovieDetailPageViewController.swift
@@ -20,35 +20,39 @@ class MovieDetailPageViewController: UIViewController {
     @IBOutlet private weak var movieMinuteLabel: UILabel!
     @IBOutlet private weak var movieGenreLabel: UILabel!
     @IBOutlet private weak var movieName: UILabel!
+    @IBOutlet weak var loadingImage: UIActivityIndicatorView!
+    
     var movieDetailPageObject : ViewToPresenterMovieDetailPageProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
         MovieDetailPageRouter.createModel(ref: self)
         movieDetailPageObject?.getMovieDetailAction(movieImdbId: movieImdbId!)
+        loadingImage.startAnimating()
+        self.movieImageView.image = UIImage(named: "noneImage")
     
     }
 }
 
 extension MovieDetailPageViewController : PresenterToViewMovieDetailPageProtocol {
     func toView(movieDetail: MovieDetail) {
-       
-        self.movieName.text = movieDetail.Title == "N/A" ? "None" : movieDetail.Title
-        if movieDetail.Poster == "N/A" {
-            self.movieImageView.image = UIImage(named: "noneImage")
-        }else{
-            let imageUrl = URL(string: movieDetail.Poster!)
-            self.movieImageView.kf.setImage(with: imageUrl)
+        DispatchQueue.main.async {
+            self.movieName.text = movieDetail.Title == "N/A" ? "None" : movieDetail.Title
+            if movieDetail.Poster == "N/A" {
+                self.movieImageView.image = UIImage(named: "noneImage")
+            }else{
+                let imageUrl = URL(string: movieDetail.Poster!)
+                self.movieImageView.kf.setImage(with: imageUrl)
+            }
+            self.loadingImage.stopAnimating()
+            self.loadingImage.isHidden = true
+            self.movieDirectorNameLabel.text = "Director by \( movieDetail.Director!)"
+            self.movieGenreLabel.text = movieDetail.Genre == "N/A" ? "None"  : movieDetail.Genre
+            self.movieWriterLabel.text = movieDetail.Writer == "N/A" ? "None"  :  movieDetail.Writer
+            self.movieLanguageLabel.text = movieDetail.Language == "N/A" ? "None"  :  movieDetail.Language
+            self.movieCountryLabel.text = movieDetail.Country == "N/A" ? "None"  :  movieDetail.Country
+            self.moviePlotTextView.text = movieDetail.Plot == "N/A" ? "None"  :  movieDetail.Plot
+            self.movieMinuteLabel.text = movieDetail.Runtime == "N/A" ? "None"  :  movieDetail.Runtime
+            self.movieImdbRatingLabel.text = movieDetail.imdbRating == "N/A" ? "None"  :  "\(movieDetail.imdbRating!)/10"
         }
-        self.movieDirectorNameLabel.text = "Director by \( movieDetail.Director!)"
-        self.movieGenreLabel.text = movieDetail.Genre == "N/A" ? "None"  : movieDetail.Genre
-        self.movieWriterLabel.text = movieDetail.Writer == "N/A" ? "None"  :  movieDetail.Writer
-        self.movieLanguageLabel.text = movieDetail.Language == "N/A" ? "None"  :  movieDetail.Language
-        self.movieCountryLabel.text = movieDetail.Country == "N/A" ? "None"  :  movieDetail.Country
-        self.moviePlotTextView.text = movieDetail.Plot == "N/A" ? "None"  :  movieDetail.Plot
-        self.movieMinuteLabel.text = movieDetail.Runtime == "N/A" ? "None"  :  movieDetail.Runtime
-        self.movieImdbRatingLabel.text = movieDetail.imdbRating == "N/A" ? "None"  :  "\(movieDetail.imdbRating!)/10"
-        
-        
-        
     }
 }
