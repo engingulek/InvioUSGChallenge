1d3baee07b1de37aee7a554e59c3a6fae835f4e9
Engin Gülek <engingulek0@gmail.com>
2022-10-06 13:26:03 +0300

    APICaller:MovieDetail

--- InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/Manager/APICaller.swift
+++ InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/Manager/APICaller.swift
@@ -31,10 +31,42 @@ class APICaller {
                     }
                 }catch{
                     completion(.failure(error))
-                    print(error.localizedDescription)
+                    
                 }
             }
         }
+    }
+    
+    func getMovieDetail(movieImdbId:String,completion: @escaping(Result<MovieDetail,Error>)->Void){
+        AF.request("\(Constanst.baseUrl)i=\(movieImdbId)&apikey=\(Constanst.apiKey)&plot=full",method: .get).response{ response in
+            if let data = response.data {
+                do {
+                    let result = try JSONDecoder().decode(MovieDetail.self, from: data)
+                    
+                    if result.Response == "True" {
+                        let movie = MovieDetail( Poster: result.Poster!,
+                                               Title: result.Title!,
+                                                Director: result.Director!,
+                                                Genre: result.Genre!,
+                                                Runtime: result.Runtime!,
+                                                Writer: result.Writer!,
+                                                Language: result.Language!,
+                                                 Country: result.Country!,
+                                                Plot: result.Plot!,
+                                                 imdbRating: result.imdbRating!,
+                                                Response: result.Response!)
+                        
+                        completion(.success(movie))
+                    }
+                }catch{
+                    completion(.failure(error))
+                    
+                }
+            
+            }
+            
+        }
+        
         
     }
     
--- InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/movieDetailPage-model/interactor/MovieDetailPageInteractor.swift
+++ InvioUSGChallengeMovieApp/InvioUSGChallengeMovieApp/movieDetailPage-model/interactor/MovieDetailPageInteractor.swift
@@ -6,39 +6,17 @@
 //
 
 import Foundation
-import Alamofire
 class MovieDetailPageInteractor : PresenterToInteractorMovieDetailPageProtocol {
     var movieDetailPagePresenter: InteractorToPresenterMovieDetailPageProtocol?
     
     func getMovieDetail(movieImdbId: String) {
-        AF.request("\(Constanst.baseUrl)i=\(movieImdbId)&apikey=\(Constanst.apiKey)&plot=full",method: .get).response{ response in
-            if let data = response.data {
-                do {
-                    let result = try JSONDecoder().decode(MovieDetail.self, from: data)
-                    
-                    if result.Response == "True" {
-                        let movie = MovieDetail( Poster: result.Poster!,
-                                               Title: result.Title!,
-                                                Director: result.Director!,
-                                                Genre: result.Genre!,
-                                                Runtime: result.Runtime!,
-                                                Writer: result.Writer!,
-                                                Language: result.Language!,
-                                                 Country: result.Country!,
-                                                Plot: result.Plot!,
-                                                 imdbRating: result.imdbRating!,
-                                                Response: result.Response!)
-                        
-                        self.movieDetailPagePresenter?.toPresenter(movieDetail: movie)
-                    }
-                }catch{
-                    
-                }
-            
+        APICaller.shared.getMovieDetail(movieImdbId: movieImdbId) { result in
+            switch result {
+            case .success(let movie):
+                self.movieDetailPagePresenter?.toPresenter(movieDetail: movie)
+            case .failure(let error):
+                print(error.localizedDescription)
             }
-            
         }
-    
-    
 }
 }
